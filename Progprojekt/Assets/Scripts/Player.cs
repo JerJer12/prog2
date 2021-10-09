f85e17f3ecf410b5ea55a34a26492b9b30caaf35
JerJer12 <71168648+JerJer12@users.noreply.github.com>
2021-10-10 01:49:21 +0200

    added player

---
+++ Progprojekt/Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private bool jumpKeyWasPressed;
+    private float horizontelInput;
+    private Rigidbody rigidbodyComp;
+    [SerializeField]private Transform groundCheck;         //így is lehetne --> public Transform groundCheck;   
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbodyComp = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed=true;
+        }
+
+        horizontelInput = Input.GetAxis("Horizontal");
+    }
+
+    // FixedUpdate is called once per every physic update
+    private void FixedUpdate() 
+    {
+        rigidbodyComp.velocity = new Vector3(horizontelInput*2, rigidbodyComp.velocity.y, 0);
+
+        if (Physics.OverlapSphere(groundCheck.position, 0.1f).Length == 1)
+        {
+            return;
+        }
+
+         if(jumpKeyWasPressed)
+        {
+            rigidbodyComp.AddForce(Vector3.up * 7, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+
+        
+    }
+
+    
+
+
+}
